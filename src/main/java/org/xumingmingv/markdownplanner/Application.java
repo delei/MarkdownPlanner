a4e1a187b5979ab71611210a6236ad5e0d3c2b1e
deleiguo <deleiguo@163.com>
2019-02-19 00:11:26 +0800

    upgrade to Spring Boot 2.0.0

--- src/main/java/org/xumingmingv/markdownplanner/Application.java
+++ src/main/java/org/xumingmingv/markdownplanner/Application.java
@@ -5,7 +5,7 @@ import java.io.File;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
